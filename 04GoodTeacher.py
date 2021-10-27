7f165d8027ed38110f77dcae7c4bbe179095394f
TrishaSarkar2564 <88806061+TrishaSarkar2564@users.noreply.github.com>
2021-10-28 10:35:34 +1100

    Add files via upload

---
+++ 04GoodTeacher.py
@@ -0,0 +1,14 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    04GoodTeacher                               |")
+print("|    Name : Trisha Sarkar                        |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+print("Hello")
+
+TeachName = input("Teacher name (try Mr Horan) " )
+if TeachName == "Mr Horan":
+    print ("You are lucky, he is a great teacher.")
+else:
+    print (TeachName + " is an ok teacher")
