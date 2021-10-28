77417b9dc6f71961212c34ef3c139b94e15229b5
TrishaSarkar2564 <88806061+TrishaSarkar2564@users.noreply.github.com>
2021-10-28 11:03:06 +1100

    Add files via upload

---
+++ 05ForLoop.py
@@ -0,0 +1,12 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    05ForLoop                                   |")
+print("|    Name : Trisha Sarkar                        |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+
+number = 1
+while number < 500:
+  print(number)
+  number += 1
