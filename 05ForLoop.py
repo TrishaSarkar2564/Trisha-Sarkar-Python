5556178574db23d945a20f90d8c5976e43774f3e
TrishaSarkar2564 <88806061+TrishaSarkar2564@users.noreply.github.com>
2021-10-28 11:11:07 +1100

    Add files via upload

---
+++ 05ForLoop.py
@@ -0,0 +1,10 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    05ForLoop                                   |")
+print("|    Name : Trisha Sarkar                        |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+
+for x in range(1, 500):
+  print(x)
