e499e15347c5869c1b4f5a8dde2bf711d0cc718c
TrishaSarkar2564 <88806061+TrishaSarkar2564@users.noreply.github.com>
2021-11-04 09:11:08 +1100

    Add files via upload

---
+++ 06WhileLoop.py
@@ -0,0 +1,21 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    06WhileLoop                                 |")
+print("|    Name : Trisha Sarkar                        |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+
+x=0
+while x == 0:
+    subject = input("What is the name of this subject ")
+    if subject == "IST":
+        print(" ")
+        print(" ")
+        print("Congratulations!!")
+        print(" ")
+        print(" ")
+        print(" ")
+        break
+    else:
+        print("Not Correct - try again")
